bec558b0f9116a3788fc636e998e84d78d93b7e0
anjaskunnummal <anjaskpkd@gmail.com>
2022-03-09 07:00:39 +0530

    minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:AuthLayoutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/auth-layout/auth-layout.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthLayoutComponent } from './auth-layout.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AuthLayoutComponent]
+})
+export class AuthLayoutModule { }
