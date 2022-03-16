a1144d2087bc2f5ea6071f79a0b1e07b144fabfe
anjaskunnummal <anjaskpkd@gmail.com>
2022-03-16 09:59:05 +0530

    logo added

---
+++ src/app/_models/request_models/book-createRequest.ts
@@ -0,0 +1,10 @@
+
+
+export class BookCreateApirequest{
+  name?:string;
+  description ?: string;
+  author_name ?: string;
+  year?:string;
+  price?:number;
+  image?:string;
+}
--- src/app/pages/add-book/add-book.component.ts
+++ src/app/pages/add-book/add-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BookCreateApirequest } from 'src/app/_models/request_models/book-createRequest';
 
 @Component({
   selector: 'app-add-book',
@@ -7,11 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookComponent implements OnInit {
 
-  Book_categories:string[]=['Study Book','Story','Poem']
+  public bookRequest:BookCreateApirequest = new BookCreateApirequest()
 
-  constructor() { }
+  Book_categories:string[]=['Study Book','Story','Poem'];
+
+  public image_URL ?:string ;
+
+  public bookForm : FormGroup = new FormGroup ({
+    name : new FormControl('',Validators.required),
+    description : new FormControl(''),
+    author_name : new FormControl(''),
+    price : new FormControl(0),
+    year : new FormControl(''),
+    category : new FormControl('')
+  })
+
+  constructor(private _snackBar:MatSnackBar) { }
 
   ngOnInit() {
+  }
+
+  Submit(){
+    if(this.bookForm.invalid){
+      this.openSnackBar('Some Field Missing')
+      return
+    }
+
+    this.createBook(this.bookForm.value)
+  }
+
+  createBook(bookform:any){
+    this.bookRequest = {
+      name : bookform.name,
+      description : bookform.description,
+      author_name : bookform.author_name,
+      price : bookform.price,
+      year : bookform.year,
+      image : this.image_URL?this.image_URL:''
+    }
+  }
+
+
+  openSnackBar(message:string){
+    this._snackBar.open(message,'close',{
+      duration:2000,
+    })
   }
 
 }
