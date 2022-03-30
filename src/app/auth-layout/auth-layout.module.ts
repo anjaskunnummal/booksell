7ee39b41d9eb2ff97ac55c06fffff41bc2af45bb
unknown <anjaskpkd@gmail.com>
2022-03-30 21:48:11 +0530

    login and register ui are completed

--- src/app/auth-layout/auth-layout.module.ts
+++ src/app/auth-layout/auth-layout.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthLayoutComponent } from './auth-layout.component';
 import { LoginComponent } from '../pages/login/login.component';
+import { RouterModule } from '@angular/router';
+import { AuthLayoutRoutes } from './auth-layout.routing';
+import { RegisterComponent } from '../pages/register/register.component';
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(AuthLayoutRoutes),
+    AngularMaterialModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent,
+  RegisterComponent]
 })
 export class AuthLayoutModule { }
