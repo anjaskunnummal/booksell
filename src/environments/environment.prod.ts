d1d9828eba01dbd13f4b462689b599625e811203
unknown <anjaskpkd@gmail.com>
2022-04-21 12:11:26 +0530

    prod url added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig : {
+    apiKey: "AIzaSyDn6Lt6ImFBqQYveQdFfD5R1_Q_7OESu9M",
+    authDomain: "midpage-5fd7b.firebaseapp.com",
+    projectId: "midpage-5fd7b",
+    storageBucket: "midpage-5fd7b.appspot.com",
+    messagingSenderId: "1050753099139",
+    appId: "1:1050753099139:web:c969ab15f6b3133e9902b6",
+    measurementId: "G-VL0R99RLR0"
+  }
 };
